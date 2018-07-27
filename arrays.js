7f31025970fe3948b9dc3b5127e148a58c47431b
Joe Muro <jmuro0321@gmail.com>
2018-07-27 18:32:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  return [element, ...array]
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
