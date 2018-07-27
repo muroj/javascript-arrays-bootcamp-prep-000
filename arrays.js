db84f4cc5079dae12f24ab39cd9f254fe35e55df
Joe Muro <jmuro0321@gmail.com>
2018-07-27 18:19:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,11 @@ function addElementToBeginningOfArray(array, element) {
 function desctructivelyAddElementToBeginningOfArray(array, element) {
   return array.unshift(element)
 }
+
+function addElementToBeginningOfArray(array, element) {
+  return [...array, element]
+}
+
+function desctructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
+}
